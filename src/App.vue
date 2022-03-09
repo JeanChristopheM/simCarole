e5fa57aae48cd442ca0baba8b08611619f228400
Jean-Christophe Molhant <jean.christophe.molhant@gmail.com>
2022-03-09 17:02:47 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Home from "./components/Home.vue";
+
+const routes: {
+  [key: string]: JSX.Element;
+} = {
+  "/": Home,
+};
+
+const currentPath = ref(window.location.hash);
+
+window.addEventListener("hashchange", () => {
+  currentPath.value = window.location.hash;
+});
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || "/"] || "NotFound";
+});
+</script>
+
+<template>
+  <Header />
+  <main class="main">
+    <component :is="currentView" />
+  </main>
+  <Footer />
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const mobileMenuToggle = ref(false);
+const handleMobileMenuBtn = (e: MouseEvent) => {
+  mobileMenuToggle.value = mobileMenuToggle.value === false ? true : false;
+};
+const menuLinks = ref(["DESTINATION", "LE WEEKEND", "LOGEMENT"]);
+</script>
+<template>
+  <header class="header">
+    <nav class="header__nav mobile">
+      <button
+        type="button"
+        class="header__nav__link mobileMenuBtn"
+        @click="handleMobileMenuBtn"
+      >
+        MENU
+      </button>
+      <div class="mobileMenu">
+        <div class="mobileMenu__linksContainer">
+          <a v-for="link in menuLinks" class="header__nav__link">{{ link }}</a>
+        </div>
+      </div>
+    </nav>
+    <nav class="header__nav desktop">
+      <a v-for="link in menuLinks" class="header__nav__link">{{ link }}</a>
+    </nav>
+  </header>
+</template>
